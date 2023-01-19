a209999b171c758afd8a0422086fcc1e7c7b91d2
for <asdfg>
2023-01-19 12:15:02 +0100

    add post endpoint to create resource

--- MinimalApiTest/Program.cs
+++ MinimalApiTest/Program.cs
@@ -1,6 +1,9 @@
+using Microsoft.AspNetCore.Mvc;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello");
+app.MapPost("/{id}", ([FromRoute] string id) => $"Create a resource with id: {id}");
 
 app.Run();
