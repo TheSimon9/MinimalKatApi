8be58fa8207afd4ec25cdf08a2e5988098597593
for <asdfg>
2023-01-19 12:02:53 +0100

    remove useless kata controller file

--- MinimalApiTest/KataController.cs
+++
@@ -1,9 +0,0 @@
-namespace MinimalApiTest;
-
-public static class KataController
-{
-    public static string Say()
-    {
-        return "Hello";
-    }
-}
--- MinimalApiTest/Program.cs
+++ MinimalApiTest/Program.cs
@@ -1,8 +1,6 @@
-using MinimalApiTest;
-
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", KataController.Say);
+app.MapGet("/", () => "Hello");
 
 app.Run();
