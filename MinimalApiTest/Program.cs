e0438dce60ae6a5b87a562284208d3b393b1614e
for <asdfg>
2023-01-27 11:57:24 +0100

    calculate ms of delivered messages

--- MinimalApiTest/Program.cs
+++ MinimalApiTest/Program.cs
@@ -2,18 +2,26 @@ using Confluent.Kafka;
 
 WebApplication.CreateBuilder(args);
 
+string Latency(string value)
+{
+    var createdAt = new DateTime(long.Parse(value.Split('-')[1]));
+    var now = DateTime.Now;
+
+    return $"{(now - createdAt).TotalMilliseconds}ms";
+}
+
 var conf = new ConsumerConfig
 {
-    GroupId = "group-name",
+    GroupId = "dispatcher",
     BootstrapServers = "localhost:9092",
     AutoOffsetReset = AutoOffsetReset.Earliest
 };
 
 var c = new ConsumerBuilder<Ignore, string>(conf).Build();
-c.Subscribe("test");
+c.Subscribe("dispatch-provider");
 
 while (true)
 {
     var cr = c.Consume(new CancellationTokenSource().Token);
-    Console.WriteLine($"Consumed message '{cr.Value}' at: '{cr.TopicPartitionOffset}'.");
+    Console.WriteLine($"Consumed message '{cr.Value}' after: '{Latency(cr.Value)}'.");
 }
